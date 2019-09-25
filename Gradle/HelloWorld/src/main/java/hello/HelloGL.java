069a70f0bebcf5095582113d299584265ca617fc
Ubuntu <ubuntu@ip-172-31-29-132.eu-west-1.compute.internal>
2019-09-25 10:05:34 +0000

    cleanup

--- Gradle/HelloWorld/src/main/java/hello/HelloGL.java
+++ Gradle/HelloWorld/src/main/java/hello/HelloGL.java
@@ -12,16 +12,7 @@ public class HelloGL {
     long eglConfigs[] = {0};
     private String brol = null;
 
-    void notestGLFromJava(long nativeWindowPtr) throws GLException {
-        egl = EGL.getEGL();
-	// egl.testAll(nativeWindowPtr);
-    }
-
     void testGLFromJava(long nativeWindowPtr) throws GLException {
-	    // System.err.println("crash on brol");
-	    // System.err.println("Length = "+brol.length());
-	    // System.err.println("did not crash on brol");
-        int[] attributes = new int[]{8,8,8,8,16,1,1};
         egl = EGL.getEGL();
 
         System.err.println("EGLDISPLAY = "+eglDisplay);
@@ -34,14 +25,6 @@ public class HelloGL {
 
 System.err.println("nativeWindow = "+nativeWindow);
 System.err.println("nativeDisplay = "+nativeDisplay);
-// egl.testGraal(nativeWindow, major, minor, attributes, eglConfigs, configCount);
-
-        if (nativeDisplay == -1l) { // error condition
-            throw new GLException(0, "Could not get native display");
-        }
-        if (nativeWindow == -1l) { // error condition
-            throw new GLException(0, "Could not get native window");
-        }
 
         eglDisplay = egl.eglGetAndInitializeDisplay(nativeDisplay);
 System.err.println("[ACC] got eglDisplay at "+eglDisplay);
@@ -51,7 +34,6 @@ System.err.println("[ACC] got eglDisplay at "+eglDisplay);
 
 
 System.err.println("[ACC] now call eglChooseConfig, eglConfigssize = "+eglConfigs.length+" and 0 = "+eglConfigs[0]+" and numconfsize = "+configCount.length+" and [0] = "+configCount[0]);
-        // if (!egl.eglChooseConfig(eglDisplay, attributes, eglConfigs, 1, configCount)) {
         if (!egl.eglChooseConfig()) {
             throw new GLException(egl.eglGetError(), "Error choosing EGL config");
         }
--- Gradle/HelloWorld/src/main/java/hello/HelloWorld.java
+++ Gradle/HelloWorld/src/main/java/hello/HelloWorld.java
@@ -3,32 +3,26 @@ package hello;
 public class HelloWorld {
 
     public static void main(String[] args) {
-	try {
-	System.err.println("HELLOWORLDMAIN, initiate instance");
-        HelloWorld hw = new HelloWorld();
-	System.err.println("HELLOWORLDMAIN, loadlib");
-	System.loadLibrary("glass_android");
-	System.err.println("HELLOWORLDMAIN, doneloadlib");
-        hw.testMe();
-	System.err.println("done testing");
-	System.err.println("graal main entry gone!");
-	} catch (Exception e) {
-		System.err.println("Whoops, got exception!");
-		e.printStackTrace();
-		System.err.println("thas was the exception!");
+        try {
+            System.err.println("HelloWorld main is called.");
+            HelloWorld hw = new HelloWorld();
+            System.err.println("HelloWorld instance created, load native lib with EGL implementations");
+            System.loadLibrary("glass_android");
+            System.err.println("HelloWorld Native lib is loaded, start testing");
+            hw.testMe();
+            System.err.println("HelloWorld done testing");
+            System.err.println("HelloWorld graal entry has done its job!");
+	} catch (Throwable e) {
+            System.err.println("Whoops, got exception!");
+            e.printStackTrace();
+            System.err.println("thas was the exception!");
 	}
     }
 
-    public void testMe() {
-        System.out.println("Hello World!!");
+    public void testMe() throws Exception {
         HelloGL hg = new HelloGL();
         long nativeWindowPtr = _getNativeHandle();
-        try {
-            hg.testGLFromJava(nativeWindowPtr);
-        }
-        catch (Throwable t) {
-            t.printStackTrace();
-        }
+        hg.testGLFromJava(nativeWindowPtr);
     }
 
     native long _getNativeHandle();
