f47aaf0ca82c9406d08fca630382fcd5c7e275b3
Johan Vos <johan@lodgon.com>
2019-11-27 13:09:55 +0100

    android specific dimensions

--- Maven/HelloFX/src/main/java/hellofx/HelloFX.java
+++ Maven/HelloFX/src/main/java/hellofx/HelloFX.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -20,16 +21,34 @@ public class HelloFX extends Application {
         ImageView imageView = new ImageView(new Image(HelloFX.class.getResourceAsStream("/hellofx/openduke.png")));
         imageView.setFitHeight(200);
         imageView.setPreserveRatio(true);
+double h = Screen.getPrimary().getVisualBounds().getHeight();
+double w = Screen.getPrimary().getVisualBounds().getWidth();
+System.err.println("HelloFX HEIGHT = "+h+", WIDTH = "+w);
+
 
         VBox root = new VBox(30, imageView, label);
         root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 640, 480);
+        Scene scene = new Scene(root, w, h);
         scene.getStylesheets().add(HelloFX.class.getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+System.err.println("Main 1 ");
+System.err.println("platform = "+System.getProperty("javafx.platform"));
+System.err.println("embedded = "+System.getProperty("embedded"));
+    System.setProperty("javafx.platform", "android");
+      System.setProperty("embedded", "monocle");
+System.setProperty("glass.platform", "Monocle");
+System.setProperty("javafx.verbose", "true");
+System.setProperty("javafx.pulseLogger", "true");
+System.setProperty("prism.verbose", "true");
+
+
+System.err.println("Main 2 ");
+System.err.println("platform = "+System.getProperty("javafx.platform"));
+System.err.println("embedded = "+System.getProperty("embedded"));
         launch(args);
     }
 
--- Maven/HelloWorld/src/main/java/hello/HelloWorld.java
+++ Maven/HelloWorld/src/main/java/hello/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.exit(0);
+        System.out.println("Start loop");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HelloWorld loop "+i);
+        }
+        System.out.println("Done Hello World!!");
     }
 
 }
