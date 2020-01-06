03a4975f40e582979d54e69bd233406faafe96a8
Johan Vos <johan.vos@gluonhq.com>
2020-01-06 11:19:42 +0100

    reproduce ios push notification issue

--- Maven/HelloFX/src/main/java/hellofx/HelloFX.java
+++ Maven/HelloFX/src/main/java/hellofx/HelloFX.java
@@ -28,34 +28,51 @@ package hellofx;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.CullFace;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 
 public class HelloFX extends Application {
 
     public void start(Stage stage) {
+        System.out.println("START");
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
         ImageView imageView = new ImageView(new Image(HelloFX.class.getResourceAsStream("/hellofx/openduke.png")));
         imageView.setFitHeight(200);
         imageView.setPreserveRatio(true);
 
-        VBox root = new VBox(30, imageView, label);
+        Button button = new Button("Click");
+        TextField textField = new TextField("Hi there");
+
+        MenuBar menuBar = new MenuBar(new Menu("Hi"));
+        menuBar.setUseSystemMenuBar(true);
+
+        VBox root = new VBox(30, imageView, label, button, textField, menuBar);
         root.setAlignment(Pos.CENTER);
         Scene scene = new Scene(root, 640, 480);
         scene.getStylesheets().add(HelloFX.class.getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
+
+        System.out.println("scene = " + scene.getUserAgentStylesheet());
     }
 
     public static void main(String[] args) {
+        System.out.println("MAIN");
         launch(args);
     }
 
